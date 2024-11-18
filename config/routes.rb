b954bb5d97cfb0d43e5219fa37fed23c748d3fdd
Eri Yawata <yawata.eri@gmail.com>
2024-11-18 22:59:41 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  # resources :users, only: [:show]
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :pets, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:create]
+    resources :reviews, only: [:index, :new, :create]
+  end
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+  resources :bookings, only: [:index, :update]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :owner do
+    resources :bookings, only: :index
+  end
 end
