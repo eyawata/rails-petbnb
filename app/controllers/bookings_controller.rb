113b9a1eda90a5b015d991868dba33d619fe677e
Eri Yawata <yawata.eri@gmail.com>
2024-11-19 20:09:55 +0900

    add booking create form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,5 +3,28 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+
+    start_date = DateTime.parse(params[:booking][:start_date])
+    end_date = DateTime.parse(params[:booking][:end_date])
+    pet_price = Pet.find(params[:pet_id]).price
+
+    @booking.user = User.last
+    @booking.start_date = start_date
+    @booking.end_date = end_date
+    @booking.pet = Pet.find(params[:pet_id])
+    @booking.total_price = pet_price * (end_date - start_date)
+
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render "", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:pet, :user, :start_date, :end_date)
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,5 +5,6 @@ class PetsController < ApplicationController
 
   def show
     @pet = Pet.find(params[:id])
+    @booking = Booking.new
   end
 end
